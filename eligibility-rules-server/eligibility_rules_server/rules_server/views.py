873e1d1edcf33d6fd7d30a0a75dfefa233a30571
Catherine Devlin <catherine.devlin@gsa.gov>
2018-02-26 20:04:43 -0500

    First integration test

--- eligibility-rules-server/eligibility_rules_server/rules_server/views.py
+++ eligibility-rules-server/eligibility_rules_server/rules_server/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class RulingsView(APIView):
     def post(self, request, program, entity, format=None):
-        return Response({'program': program, 'entity': entity, 'payload': request.data, 'results': ['ok', ]})
+        return Response({'program': program,
+                         'entity': entity,
+                         'payload': request.data,
+                         'results': ['ok', ]})
 
     def get(self, request, program, entity, format=None):
-        return Response({'program': program, 'entity': entity, 'results': ['ok', ]})
+        return Response({'program': program,
+                         'entity': entity,
+                         'results': ['ok', ]})
